9b594723b08db7ac76b8e0b74447630bcc711b25
fasjesus <developerflavia@gmail.com>
2024-10-15 17:18:44 -0300

    final db

--- DB/hotel_hub.sql
+++ DB/hotel_hub.sql
@@ -10,31 +10,27 @@ CREATE TABLE CLIENTE (
 	idade INTEGER,
 	senha VARCHAR(100),
 	email VARCHAR(100),
+    telefone VARCHAR(100),
 	PRIMARY KEY(id_cliente)
 );
 
 CREATE TABLE HOTEL (
     id_hotel INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome_hotel VARCHAR (15) NOT NULL,
     telefone_hotel  VARCHAR (15) NOT NULL,
     estado_hotel  VARCHAR (15) NOT NULL,
     cidade_hotel VARCHAR (15) NOT NULL,
     endereco_hotel VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE VALOR (
-    id_valor INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    valor DECIMAL
-);
-
 CREATE TABLE QUARTO (
     id_quarto INT PRIMARY KEY AUTO_INCREMENT,
     fk_id_hotel INT NOT NULL,
-    fk_id_valor INT NOT NULL,
     numeroQuarto VARCHAR(10) NOT NULL,
     tipoQuarto VARCHAR(50) NOT NULL, 
-    Status VARCHAR(50) NOT NULL,
-    FOREIGN KEY (fK_id_hotel) REFERENCES HOTEL(id_hotel) ON DELETE CASCADE,
-    FOREIGN KEY (fK_id_valor) REFERENCES VALOR(id_valor) ON DELETE CASCADE
+    statusQuarto VARCHAR(20) NOT NULL,
+    valor_quarto DECIMAL,
+    FOREIGN KEY (fK_id_hotel) REFERENCES HOTEL(id_hotel) ON DELETE CASCADE
 );
 
 CREATE TABLE METODO_PAGAMENTO (
@@ -46,10 +42,19 @@ CREATE TABLE RESERVA (
     id_reserva INT PRIMARY KEY AUTO_INCREMENT,
     fK_id_cliente INT NOT NULL,
     fK_id_quarto INT NOT NULL,
-    fk_metodo_pagamento INT NOT NULL,
     dataCheckIn DATE NOT NULL,
     dataCheckOut DATE NOT NULL,
+    statusReserva VARCHAR(20) NOT NULL,
     FOREIGN KEY (fK_id_quarto) REFERENCES QUARTO(id_quarto) ON DELETE CASCADE,
-    FOREIGN KEY (fK_id_cliente) REFERENCES CLIENTE(id_cliente) ON DELETE CASCADE,
-    FOREIGN KEY (fk_metodo_pagamento) REFERENCES METODO_PAGAMENTO(id_metodo_pagamento) ON DELETE CASCADE
+    FOREIGN KEY (fK_id_cliente) REFERENCES CLIENTE(id_cliente) ON DELETE CASCADE
 );
+
+CREATE TABLE PAGAMENTO_RECEBIMENTO (
+	id_fatura INT PRIMARY KEY AUTO_INCREMENT,
+    fK_reserva INT NOT NULL,
+    fK_metodo_pagamento INT NOT NULL,
+    data_emissao DATE NOT NULL,
+    valor_fatura DECIMAL,
+    FOREIGN KEY (fK_reserva) REFERENCES RESERVA(id_reserva) ON DELETE CASCADE,
+    FOREIGN KEY (fk_metodo_pagamento) REFERENCES METODO_PAGAMENTO(id_metodo_pagamento) ON DELETE CASCADE
+)
